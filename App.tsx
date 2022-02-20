c0c2d3caa106cbaa8997b284e933e36f8d3dc2ba
Caio Poyares <capoyares@gmail.com>
2022-02-20 20:01:32 -0300

    add ui library react native paper

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
+import { Provider as PaperProvider } from 'react-native-paper'
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <PaperProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </PaperProvider>
       </SafeAreaProvider>
-    );
+    )
   }
 }
--- screens/Emergency.tsx
+++ screens/Emergency.tsx
@@ -1,32 +1,68 @@
-import { StyleSheet, Text } from "react-native";
-import { View } from "../components/Themed";
+import { StyleSheet } from 'react-native'
+import { View } from '../components/Themed'
+import { Title, Card, Button } from 'react-native-paper'
+
+const emergencies = [
+  {
+    title: 'Parada respiratória',
+    address: 'Rua Nove de Julho, 183',
+  },
+  {
+    title: 'Miocárdio',
+    address: 'Rua Barão do Cotegipe, 39',
+  },
+  {
+    title: 'Arritmia cardíaca',
+    address: 'Avenida Bandeirantes, 1231',
+  },
+]
 
 export default function Emergency() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Emergência</Text>
-      <View
+      <Title style={styles.title}>Chamados</Title>
+      <View style={{ margin: 10 }}>
+        {emergencies.map((emergency: any) => (
+          <Card elevation={1} style={{ marginBottom: 10 }}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ flex: 1 }}>
+                <Card.Title
+                  title={emergency.title}
+                  subtitle={emergency.address}
+                />
+              </View>
+              <View style={{ justifyContent: 'center' }}>
+                <Card.Actions>
+                  <Button>Ver mais</Button>
+                </Card.Actions>
+              </View>
+            </View>
+          </Card>
+        ))}
+      </View>
+
+      {/* <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
-      />
+      /> */}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    marginTop: 15,
+    marginLeft: 10,
+    fontWeight: 'bold',
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: '80%',
   },
-});
+})
